8979742351a08082a278c2bb4be7ee9bcc820823
bubblguy <santiago@usebubbl.app>
2023-01-31 20:27:17 -0500

    Initial Commit

---
+++ OriginalFirebase/OriginalFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  OriginalFirebaseApp.swift
+//  OriginalFirebase
+//
+//  Created by Santiago Pozuelo on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct OriginalFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OriginalFirebaseUITests/OriginalFirebaseUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  OriginalFirebaseUITestsLaunchTests.swift
+//  OriginalFirebaseUITests
+//
+//  Created by Santiago Pozuelo on 1/31/23.
+//
+
+import XCTest
+
+class OriginalFirebaseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
